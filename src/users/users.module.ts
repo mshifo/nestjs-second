8d28f144d1fff966ebf48912d082ae05f7ac8509
Mahmoud Elshafaey <mahmoud.elshafaey@linkdev.com>
2023-08-16 12:58:15 +0300

    set auth guard globally

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,19 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { LoggerMiddleware } from './logger.middleware';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { APP_GUARD } from '@nestjs/core/constants';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      // Enable authentication globally
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   exports: [UsersService],
 })
 export class UsersModule implements NestModule {
