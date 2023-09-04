bb98baa421e25c33c67171af998c07305df1eb37
Mahmoud Elshafaey <mahmoud.elshafaey@linkdev.com>
2023-09-04 15:34:50 +0300

    install swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,16 @@ async function bootstrap() {
       whitelist: true, //automatically remove those without any decorator in the validation class
     }),
   ); //set globally to every route
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port');
